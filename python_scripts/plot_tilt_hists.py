dc661339bb8f6998d0222f5befb965a332ec8aba
KhangPham0 <phamkhang0o0@gmail.com>
2022-09-14 13:13:08 -0500

    replace sys module with argparse and use plot_specific_region.py to plot

--- python_scripts/plot_tilt_hists.py
+++ python_scripts/plot_tilt_hists.py
@@ -2,58 +2,64 @@
 
 import ROOT
 import numpy as np
-import sys
 import re
+import argparse
+import plot_specific_region as psr
 
-# check for number of files
-num_files= len(sys.argv) - 1
+# to plot histograms with ROOT
+def plotHists(inFiles, bins):
 
-# load in all files
-files = np.empty(num_files, dtype=np.ndarray)
-for i in range(num_files):
-    files[i] = np.loadtxt(sys.argv[i+1])
+    # number of files
+    nFiles = len(inFiles)
 
-# make a canvas, an array of hist(s)
-c1 = ROOT.TCanvas('c1', 'Tilt Combinations', 300, 0, 1300, 1000) # (name, title, top left x coord, top left y coord, window x width, window y width)
-hists = np.empty(num_files, dtype=ROOT.TH1F)
+    # load in all files and get values of H out of the filenames
+    filesArr = np.empty(nFiles, dtype=np.ndarray)
+    Hs = np.empty(nFiles)
+    for i in range(nFiles):
 
-# fill the hist(s) and also find the max value of Y-axis of plot(s)
-binMax = 0
-tmpMax = 0
-for j in range(num_files):
-    hists[j] = ROOT.TH1F(f'{j}', sys.argv[j+1], 1000, -150, 300) # (name, title, numOfBins, xlow, xup)
+        filesArr[i] = np.loadtxt(inFiles[i])
 
-    # get values of H from file names to label the histogram
-    H, alpha = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", sys.argv[j+1])
-    hists[j].SetYTitle(f'{float(H):.2f}')
+        # get values of H from file names to label the histogram
+        Hs[i], alpha_tmp = re.findall(r"[-+]?(?:\d*\.\d+|\d+)", inFiles[i].name)
+    
+    # note that -300 and 300 are default values of SPS eventbuilder for the full histo range
+    xlows = np.full(nFiles, -300)
+    xhighs = np.full(nFiles, 300)
+    canvas, histos = psr.plotThem(nFiles, filesArr, Hs, xlows, xhighs, bins)
 
-    # some formatting for y-axis title so that it's visible
-    hists[j].SetTitleOffset(0.1, 'Y')
-    hists[j].SetTitleSize(0.3,'Y')
-    hists[j].GetYaxis().SetLabelSize(0)
-    #hists[j].GetXaxis().SetLabelSize(0)
+    return canvas
 
-    # fill histogram with values from given file(s)
-    for val in files[j]:
-        hists[j].Fill(val)
-    tmpMax = hists[j].GetMaximum()
-    if binMax < tmpMax: binMax = tmpMax
 
-# plot hist(s)
-c1.Divide(1,num_files,0,0) # divide canvas into chunks based on no. of files
-for k in range(num_files):
-    c1.cd(k+1)
-    #ROOT.gPad.SetLogy() # uncomment to set y-axis to log scale
-    #hists[k].SetAxisRange(0, int(1.1*binMax), 'Y') # same purpose as the line below
-    hists[k].SetMaximum(int(1.1*binMax)) # set max range to 10 percent higher than the tallest peak
-    #hists[k].Draw("PE1X0") # draw unconnected points with error bars
-    #hists[k].Draw("EHISTO") # draw connected points with error bars
-    hists[k].Draw()
+#####################################################
+###                                               ###
+###                 MAIN FUNCTION                 ###
+###                                               ###
+#####################################################
+# to control what the whole program does
+def main(files, nbinsx):
 
-ROOT.gStyle.SetOptStat(0)
-ROOT.gStyle.SetOptFit(1111)
-c1.Update()
+    results = plotHists(files, nbinsx)
 
-# this line is needed for the canvas to stay on screen
-# alternatively, run this script with "python3 -i ..."
-ROOT.gApplication.Run()
+    return results
+
+
+# everything starts here
+if __name__ == '__main__':
+
+    # handle the command line arguments
+    parser = argparse.ArgumentParser(description="Fit peak or peaks within a specific region")
+    parser.add_argument('files', type=argparse.FileType('r'), nargs='+', help="text files of x values")
+    parser.add_argument("-bin", "--nbinsx", type=int, default=-10, help="number of bins; if no value\
+                                                                    is given, bins = xhigh - xlow is used")
+
+    # pass all of the arguments into args
+    args = parser.parse_args()
+
+    #print(args.files[0].name)
+    
+    # pass the args into main function
+    results = main(**vars(args))
+
+    # this line is needed for the canvas to stay on screen
+    # alternatively, run this script with "python3 -i ..."
+    ROOT.gApplication.Run()
